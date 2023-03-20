e093da44ce5e68814e96a456a669f7325e23e24b
Ahmed Wael <AhmedWael216@protonmail.com>
2023-03-20 15:52:53 +0200

    Added Table constructor

--- src/main/java/DB/Table.java
+++ src/main/java/DB/Table.java
@@ -1,4 +1,22 @@
 package DB;
 
+import java.util.Hashtable;
+
 public class Table {
+    String name;
+    Row prototype;
+
+    public Table (String strTableName,
+                  String strClusteringKeyColumn,
+                  Hashtable<String,String> htblColNameType,
+                  Hashtable<String,String> htblColNameMin,
+                  Hashtable<String,String> htblColNameMax )
+            throws DBAppException{
+        this.name=name;
+        try {
+            this.prototype = new Row(strClusteringKeyColumn,htblColNameType,htblColNameMin,htblColNameMax);
+        }catch (Exception e){
+            System.out.println(e.getMessage());
+        }
+    }
 }
---
+++ src/test/java/DB/TableTest.java
@@ -0,0 +1,12 @@
+package DB;
+
+import org.junit.jupiter.api.BeforeEach;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class TableTest {
+    @BeforeEach
+    void setUp() {
+
+    }
+}
