0aa2faab6b532aba76b356899e822fc94178fc74
Omar Mohammad <89214824+OmarMUhammed03@users.noreply.github.com>
2023-03-23 22:25:24 +0200

    Update Table.java

--- src/main/java/DB/Table.java
+++ src/main/java/DB/Table.java
@@ -1,37 +1,5 @@
 package DB;
 
-import java.io.File;
-import java.io.IOException;
-import java.util.Hashtable;
-
-public class Table {
-    private String name;
-    private Row prototype;
-    private int size;
-
-    public Table(String strTableName,
-                 String strClusteringKeyColumn,
-                 Hashtable<String, String> htblColNameType,
-                 Hashtable<String, String> htblColNameMin,
-                 Hashtable<String, String> htblColNameMax)
-            throws DBAppException, RuntimeException {
-        this.name = strTableName;
-        //TODO load DBName from DBApp.config
-        String DBName = "DB1";
-
-        //Creating a Directory for the table
-        new File(DBName + "/" + name).mkdir();
-        //Creating a Metadata file for the table
-        try {
-            new File(DBName + "/" + name + "/" + "Metadata.csv").createNewFile();
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
-
-        //TODO add column data to csv file
-
-package DB;
-
 import com.opencsv.CSVWriter;
 
 import java.io.*;
@@ -69,6 +37,7 @@ public class Table {
             throw new RuntimeException(e);
         }
 
+        //TODO add column data to csv file
         writer.writeNext(new String[]{"TableName","ColumnName", "ColumnType", "ClusteringKey", "IndexName", "IndexType", "min", "max"});
 
         // add the primary key column
@@ -93,30 +62,6 @@ public class Table {
         }
         size = 0;
     }
-    public int getSize() {
-        return this.size;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public static void main(String[] args) throws DBAppException {
-        try {
-            new Table("test", null, null, null, null);
-        } catch (DBAppException e) {
-            throw new RuntimeException(e);
-        }
-    }
-}
-        try {
-            //this.prototype = new Row(strClusteringKeyColumn,htblColNameType);
-        } catch (Exception e) {
-            System.out.println(e.getMessage());
-        }
-        size = 0;
-    }
-
     public int getSize() {
         return this.size;
     }
