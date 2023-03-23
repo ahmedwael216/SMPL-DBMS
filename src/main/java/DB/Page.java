42ad92138de808c61c1850b1e5e9f7311d5192fc
amir-awad <amirawad004@gmail.com>
2023-03-23 14:53:38 +0200

    Adding Page constructor.

--- src/main/java/DB/Page.java
+++ src/main/java/DB/Page.java
@@ -1,9 +1,24 @@
 package DB;
 
-public class Page {
+import java.io.Serializable;
+import java.util.Vector;
+
+public class Page implements Serializable {
+    Vector<Record> records;
+
+    public Page() {
+        records = new Vector<Record>();
+    }
+
+    public Vector<Record> getRecords() {
+        return records;
+    }
 
     public static void main(String[] args) {
         int maxRecordsCountinPage = DbApp.maxRecordsCountinPage;
         System.out.println(maxRecordsCountinPage);
     }
+
+    // insert sorted => binary search inside the page => return null if inserted => return the last record if page is full
+    
 }
