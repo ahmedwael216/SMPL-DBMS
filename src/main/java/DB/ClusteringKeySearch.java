ed87329c849fd5f9c0dda48b076ff6408f104d1e
Ibrahim-Soltan <ibrahim.soltan918@gmail.com>
2023-05-14 14:40:45 +0300

    Clustering key Search

--- src/main/java/DB/ClusteringKeySearch.java
+++ src/main/java/DB/ClusteringKeySearch.java
@@ -22,7 +22,6 @@ public class ClusteringKeySearch extends SearchStrategy{
         Page page = TablePersistence.deserialize(pageno,tableName);
 
 
-
         DBVector<Record> records = page.getRecords();
 
         Record record = (Record) prototype.clone();
@@ -33,31 +32,77 @@ public class ClusteringKeySearch extends SearchStrategy{
         int recordno = records.binarySearch(record);
 
 
+        switch (operator){
+            case "=": ClusteringKeySearch.equal(pageno,recordno,tableName,result);
+            case ">": ClusteringKeySearch.greaterThan(pageno,recordno,tableName,result);
+            case ">=": ClusteringKeySearch.greaterThanOrEqual(pageno,recordno,tableName,result);
+            case  "<": ClusteringKeySearch.lessThan(pageno,recordno,tableName,result);
+            case "<=": ClusteringKeySearch.lessThanOrEqual(pageno,recordno,tableName,result);
+            case "!=": ClusteringKeySearch.notEqual(pageno,recordno,tableName,result);
+        }
+
         return result;
     }
 
 
-    public static void greaterThan(int pageno, int recordno, String tableName,DBVector<Record> result){
-        
-    }
+    public static void greaterThan(int pageno, int recordno, String tableName,DBVector<Record> result) throws IOException, ClassNotFoundException {
+        if(recordno>=0) recordno++;
+        else recordno = -(recordno+1);
 
-    public static void lessThan(int pageno, int recordno, String tableName,DBVector<Record> result){
+        int numberOfPages = TablePersistence.getNumberOfPagesForTable(tableName);
 
-    }
-
-    public static void equal(int pageno, int recordno, String tablename, DBVector<Record> result){
-
-    }
-    public static void notEqual(int pageno, int recordno, String tablename, DBVector<Record> result){
-
-    }
-
-    public static void greaterThanOrEqual(int pageno, int recordno, String tablename, DBVector<Record> result){
+        for (;pageno<numberOfPages;pageno++){
+            Page page = TablePersistence.deserialize(pageno,tableName);
+            DBVector<Record> records = page.getRecords();
+            for(;recordno<records.size();recordno++){
+                result.add(records.get(recordno));
+            }
+            recordno = 0;
+        }
 
     }
 
-    public static void lessThanOrEqual(int pageno, int recordno, String tablename, DBVector<Record> result){
+    public static void lessThan(int pageno, int recordno, String tableName,DBVector<Record> result) throws IOException, ClassNotFoundException {
+        if(recordno>=0) recordno--;
+        else recordno = -(recordno+2);
 
+        if(recordno<0){
+            pageno--;
+            recordno= TablePersistence.deserialize(pageno,tableName).getRecords().size()-1;
+        }
+
+        for(int i=0;i<pageno;i++){
+            Page page = TablePersistence.deserialize(i,tableName);
+            DBVector<Record> records = page.getRecords();
+            for(int j=0;j<recordno;j++){
+                result.add(records.get(j));
+            }
+        }
+
+    }
+
+    public static void equal(int pageno, int recordno, String tableName, DBVector<Record> result) throws IOException, ClassNotFoundException {
+        if(recordno<0) return;
+        Page page = TablePersistence.deserialize(pageno,tableName);
+        DBVector<Record> records = page.getRecords();
+        result.add(records.get(recordno));
+
+
+    }
+    public static void notEqual(int pageno, int recordno, String tableName, DBVector<Record> result) throws IOException, ClassNotFoundException {
+        ClusteringKeySearch.lessThan(pageno,recordno,tableName,result);
+        ClusteringKeySearch.greaterThan(pageno,recordno,tableName,result);
+
+    }
+
+    public static void greaterThanOrEqual(int pageno, int recordno, String tableName, DBVector<Record> result) throws IOException, ClassNotFoundException {
+        ClusteringKeySearch.equal(pageno,recordno,tableName,result);
+        ClusteringKeySearch.greaterThan(pageno,recordno,tableName,result);
+    }
+
+    public static void lessThanOrEqual(int pageno, int recordno, String tableName, DBVector<Record> result) throws IOException, ClassNotFoundException {
+        ClusteringKeySearch.equal(pageno,recordno,tableName,result);
+        ClusteringKeySearch.lessThan(pageno,recordno,tableName,result);
     }
 
 
